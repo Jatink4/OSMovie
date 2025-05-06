f2561f13a7beb4480f833167f47744131d6c52af
Jatin Kumar <153261052+Jatink4@users.noreply.github.com>
2025-05-06 08:03:40 +0530

    Update api.js

--- project movie/assets/js/api.js
+++ project movie/assets/js/api.js
@@ -1,4 +1,4 @@
-const api_key='35b70cec2e37bfcf1da0f87d003b230b';
+const api_key='API_TMDB';
 const imageBaseURL='https://image.tmdb.org/t/p/';
 /**fetch data from a server using the url and passes the result in json data to a callback function along with an optional parameter if has optionalparam */
 const fetchDataFromServer=function(url,callback,optionalParam){
